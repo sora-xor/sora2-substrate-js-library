f74d6f104b38993878c69bd684da731abbfdf469
Stefan Popov <stefanpopov@list.ru>
2020-12-02 14:15:01 +0400

    Fix issues

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,14 @@ module.exports = {
     '@sora-substrate/api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
     '@sora-substrate/types(.*)$': '<rootDir>/packages/types/src/$1',
     '@sora-substrate/type-definitions(.*)$': '<rootDir>/packages/type-definitions/src/$1',
+    '@sora-substrate/util(.*)$': '<rootDir>/packages/util/src/$1',
   },
   modulePathIgnorePatterns: [
     '<rootDir>/build',
     '<rootDir>/packages/api/build',
     '<rootDir>/packages/api-derive/build',
     '<rootDir>/packages/types/build',
-    '<rootDir>/packages/type-definitions/build'
+    '<rootDir>/packages/type-definitions/build',
+    '<rootDir>/packages/util/build'
   ]
 }
