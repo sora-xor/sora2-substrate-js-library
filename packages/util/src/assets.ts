b778ebbfd0e0adb6f826f800efbf57afcde70b89
Stefan Popov <stefanpopov@list.ru>
2020-12-03 17:54:37 +0400

    Modify wallet api utils

--- packages/util/src/assets.ts
+++ packages/util/src/assets.ts
@@ -1,13 +1,20 @@
 import { ApiPromise } from '@polkadot/api'
 import { Codec } from '@polkadot/types/types'
 
+export interface AccountAsset {
+  address: string;
+  balance: string;
+  symbol?: string;
+  decimals?: number;
+}
+
 export interface Asset {
   address: string;
   symbol: string;
   decimals: number;
 }
 
-export enum Symbols {
+export enum KnownSymbols {
   XOR = 'XOR',
   DOT = 'DOT',
   KSM = 'KSM',
@@ -19,38 +26,38 @@ export enum Symbols {
 export const KnownAssets: Array<Asset> = [
   {
     address: '0x0200000000000000000000000000000000000000000000000000000000000000',
-    symbol: Symbols.XOR,
+    symbol: KnownSymbols.XOR,
     decimals: 18
   },
   {
     address: '0x0200010000000000000000000000000000000000000000000000000000000000',
-    symbol: Symbols.DOT,
+    symbol: KnownSymbols.DOT,
     decimals: 10
   },
   {
     address: '0x0200020000000000000000000000000000000000000000000000000000000000',
-    symbol: Symbols.KSM,
+    symbol: KnownSymbols.KSM,
     decimals: 12
   },
   {
     address: '0x0200030000000000000000000000000000000000000000000000000000000000',
-    symbol: Symbols.USD,
+    symbol: KnownSymbols.USD,
     decimals: 18
   },
   {
     address: '0x0200040000000000000000000000000000000000000000000000000000000000',
-    symbol: Symbols.VAL,
+    symbol: KnownSymbols.VAL,
     decimals: 18
   },
   {
     address: '0x0200050000000000000000000000000000000000000000000000000000000000',
-    symbol: Symbols.PSWAP,
+    symbol: KnownSymbols.PSWAP,
     decimals: 18
   }
 ]
 
 export async function getAssetInfo (api: ApiPromise, accountAddress: string, assetAddress: string): Promise<Codec> {
-  const xor = KnownAssets.find(asset => asset.symbol === Symbols.XOR)
+  const xor = KnownAssets.find(asset => asset.symbol === KnownSymbols.XOR)
   const isNative = assetAddress === xor.address
   const asset = await (
     isNative ? api.query.system.account(accountAddress) : api.query.tokens.accounts(accountAddress, assetAddress)
