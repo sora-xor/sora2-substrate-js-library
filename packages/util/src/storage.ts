fb08c9c35d83b1275a4df33beac27af9d40935cf
Stefan Popov <stefanpopov@list.ru>
2020-12-04 22:11:12 +0400

    Implement storage

---
+++ packages/util/src/storage.ts
@@ -0,0 +1,23 @@
+export class Storage {
+  constructor () {}
+
+  public all () {
+    return Object.entries(localStorage).filter(([key]) => key.startsWith('sora'))
+  }
+
+  public get (key: string): string {
+    return localStorage.getItem(`sora.${key}`)
+  }
+
+  public set (key: string, value: any): void {
+    localStorage.setItem(`sora.${key}`, value)
+  }
+
+  public remove (key: string): void {
+    localStorage.removeItem(`sora.${key}`)
+  }
+
+  public clear (): void {
+    this.all().forEach(([key]) => localStorage.removeItem(key))
+  }
+}
