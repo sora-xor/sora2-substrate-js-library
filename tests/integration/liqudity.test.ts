f7b926c6d5c8811a4522bc3a5764d414ff29389c
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-01-25 10:58:48 +0300

    update implementation

--- tests/integration/liqudity.test.ts
+++ tests/integration/liqudity.test.ts
@@ -1,25 +1,23 @@
 import { DexApi, KnownAssets, KnownSymbols } from '@sora-substrate/util'
-import { TestUtils, getAsset, updateBalance, createKeyring, setupEnvironment } from './util'
+import { TestApi } from '../util'
 import { ApiPromise } from '@polkadot/api'
 import { strictEqual } from 'assert'
 
 const TEST_ENDPOINT = 'wss://ws.stage.sora2.soramitsu.co.jp'
 const sudoSeed = "scissors spread water arrive damp face amazing shrug warfare silk dry prison"
 const dexSeed = "era actor pluck voice frost club gallery palm moment empower whale flame"
+const testUserSeed = "nominee hundred leader math hurt federal limit frozen flag skirt sentence hello"
 
 describe('Liqudity test function', (): void => {
-  let utils: TestUtils
-  let dexApi: DexApi
+  let testApi: TestApi
   beforeAll(async (done) => {
-    dexApi = new DexApi(TEST_ENDPOINT)
-    utils = new TestUtils
-    await dexApi.initialize()
-    await setupEnvironment(TEST_ENDPOINT, dexApi, sudoSeed, dexSeed)
+    testApi = new TestApi(TEST_ENDPOINT)
+    await testApi.initialize()
+    await testApi.setupEnvironment(sudoSeed, testUserSeed)
     done()
   }, 10_000_000)
   afterAll(async (done) => {
-    await dexApi.disconnect()
-    await utils.disconnect()
+    await testApi.disconnect()
     done()
   })
   it('Add liqudity', async (): Promise<void> => {
