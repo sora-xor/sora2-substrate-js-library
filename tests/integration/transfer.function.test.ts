a83893502ab9ec680b8f0b90efcc2c36d60f30b9
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-02-01 22:36:47 +0300

    improve implementation

---
+++ tests/integration/transfer.function.test.ts
@@ -0,0 +1,71 @@
+import { connection, FPNumber, KnownAssets, KnownSymbols } from '@sora-substrate/util'
+import { TestApi } from '../util'
+import { ENV } from '../env'
+
+const env = ENV.STAGE
+const TEST_ENDPOINT = env.URL
+const sudoSeed = env.SUDO_SEED
+const dexSeed = env.DEX_SEED
+const testUserSeed = env.USER_SEED
+
+describe('Test Transfer function', (): void => {
+  let testApi: TestApi
+  jest.setTimeout(80_000)
+  beforeAll(async (done) => {
+    testApi = new TestApi()
+    await connection.open(TEST_ENDPOINT)
+    await testApi.initialize()
+    await testApi.setupEnvironment(sudoSeed, testUserSeed, dexSeed)
+    done()
+  }, 10_000_000)
+  afterAll(async (done) => {
+    await connection.close()
+    done()
+  })
+  beforeEach(async (done) => {
+    testApi.logout()
+    done()
+  })
+  it.only.each([
+    [KnownSymbols.XOR, 10],
+    [KnownSymbols.PSWAP, 10],
+    [KnownSymbols.VAL, 10]
+  ])('Transfer %s amount= %s', async (assetSymbol, amount): Promise<void> => {
+    //Given
+    let transferAmount: number
+    const receiverSeed = testApi.createSeed().seed
+    const dexKeyPair = testApi.createKeyring('sr25519', 'dex', dexSeed)
+    const receiver = testApi.createKeyring('sr25519', 'receiver', await receiverSeed)
+    const senderSeed = testApi.createSeed().seed
+    const sender = testApi.createKeyring('sr25519', "Sender", senderSeed)
+    const xor = KnownAssets.get(KnownSymbols.XOR)
+    const inputAsset = KnownAssets.get(assetSymbol)
+
+    testApi.importAccount(senderSeed,"Test","1")
+    await testApi.mintTokens(dexKeyPair, amount, sender, inputAsset)
+    if (assetSymbol !== xor.symbol){
+      await testApi.mintTokens(dexKeyPair, 1, sender, xor)
+    }
+    await testApi.getKnownAccountAssets()
+
+    const senderAccountAsset = testApi.accountAssets.find(i => i.symbol === assetSymbol)
+    const balanceBeforeTransfer = senderAccountAsset.balance
+
+    const networkFee = await testApi.getTransferNetworkFee(inputAsset.address, receiver.address, amount)
+
+    if (assetSymbol == xor.symbol){
+      transferAmount = amount - +networkFee
+    } else {
+      transferAmount = amount
+    }
+
+    //Then
+    await testApi.transfer(inputAsset.address, receiver.address, transferAmount)
+    
+    //When
+    await testApi.updateBalance(inputAsset, balanceBeforeTransfer)    
+    const result = +balanceBeforeTransfer - amount
+
+    expect(+senderAccountAsset.balance).toBe(result)
+  })
+})
--- tests/util/TestApi.ts
+++ tests/util/TestApi.ts
@@ -1,27 +1,20 @@
 import * as bip39 from 'bip39'
 import { ApiPromise } from '@polkadot/api'
-import { AccountAsset, Asset, DexApi, KnownAssets, KnownSymbols } from '@sora-substrate/util'
+import { Asset, DexApi, KnownAssets, KnownSymbols, FPNumber } from '@sora-substrate/util'
 import { Keyring } from '@polkadot/api'
 import { KeypairType } from '@polkadot/util-crypto/types'
 import { KeyringPair } from '@polkadot/keyring/types'
-import { FPNumber } from '@sora-substrate/util'
 
 export class TestApi extends DexApi {
-
-  constructor (endpoint?: string) {
-    super(endpoint)
-  }
 
   private async innerSubmitExtrinsic(api: ApiPromise, extrinsic: any, signer: any, finishCallback: any): Promise<void> {
     // this is quick example, refer to https://polkadot.js.org/docs/api/cookbook/tx and https://polkadot.js.org/docs/api/start/api.tx.subs
     const unsub = await extrinsic.signAndSend(signer, (result: any) => {
-      console.log(`Current status is ${result.status}`);
-
+      console.log(`Current status is ${result.status}`)
       if (result.status.isInBlock) {
-        console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+        console.log(`Transaction included at blockHash ${result.status.asInBlock}`)
       } else if (result.status.isFinalized) {
-        console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
-
+        console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`)
         result.events.forEach(({ phase, event: { data, method, section } }: any) => {
           console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
           if (section === 'system' && method === 'ExtrinsicFailed') {
@@ -37,133 +30,190 @@ export class TestApi extends DexApi {
           } else {
             console.log("Transaction success.")
           }
-        });
+        })
 
-        unsub();
-        finishCallback();
+        unsub()
+        finishCallback()
       }
-    });
+    })
   }
 
   public async customSubmitExtrinsic(extrinsic: any, signer: any, debugMessage = ''): Promise<void> {
-    console.log(`\nSubmit extrinsic: ${debugMessage}\n`);
+    console.log(`\nSubmit extrinsic: ${debugMessage}\n`)
     return new Promise((resolve, _reject) => {
-      this.innerSubmitExtrinsic(this.api, extrinsic, signer, resolve);
-    });
+      this.innerSubmitExtrinsic(this.api, extrinsic, signer, resolve)
+    })
   }
-
   /**
- * Makes 3 attempts to update the balance, if the balance is not updated during this time makes reject
- * @param inputAssetSymbol 
- * @param inputAssetsBeforeSwap 
- */
-  public async updateBalance(inputAssetSymbol: KnownSymbols, inputAssetsBeforeSwap: string): Promise<void> {
-    for (let i = 0; i < 3; i++) {
-      if (inputAssetsBeforeSwap == this.accountAssets.find(i => i.symbol === inputAssetSymbol).balance)
+   * Tries to update the balance 10 times, if the balance is not updated, the pledge will be rejected.
+   * @param asset
+   * @param balanceBefore The amount with which the balance will be compared after the update
+   */
+  public async updateBalance(asset: Asset, balanceBefore: string): Promise<void> {
+    for (let i = 0; i <= 10; i++) {
+      if (balanceBefore == this.accountAssets.find(i => i.symbol === asset.symbol).balance){
         await this.updateAccountAssets()
-      else if (i==3 && inputAssetsBeforeSwap == this.accountAssets.find(i => i.symbol === inputAssetSymbol).balance) {
-        console.log("Balance is not updated after 3 times") 
+        await new Promise((r) => setTimeout(r, 3000))
+      }
+      else if (i==10 && balanceBefore == this.accountAssets.find(i => i.symbol === asset.symbol).balance) {
+        console.log("Balance is not updated after 10 times") 
         Promise.reject
-      } else continue
+      } else {
+        Promise.resolve
+        break
+      }
     }
   }
 
   /**
-   * 
-   * @param cryptoType 
-   * @param userName 
-   * @param mnemonic If you do not pass this parameter the key pair will be generated from a random mnemonic
-   * @returns
+   * Create KeyringPair from mnemonic phrase or generate random if mnemonic has not been provided.
+   * @param cryptoType type like "ed25519" | "sr25519" | "ecdsa" | "ethereum"
+   * @param userName Just name.
+   * @param mnemonic If passed generates a KeyringPair from this mnemonic.
+   * @param mnemonicLength If passed generates a KeyringPair from mnemonic of a specific length(12, 18 or 24), default - 12 words
+   * @returns KeyringPair
    */
-  public createKeyring(cryptoType: KeypairType, userName: string, mnemonic?: string): KeyringPair {
+  public createKeyring(cryptoType: KeypairType, userName: string, mnemonic?: string, mnemonicLength?: number): KeyringPair {
     const keyring = new Keyring({ type: cryptoType });
     if (mnemonic) {
       return keyring.addFromMnemonic(mnemonic, { name: userName })
     } 
     else {
-      const generatedMnemonic = bip39.generateMnemonic()
+      const generatedMnemonic = bip39.generateMnemonic(!mnemonicLength ? 12 : mnemonicLength)
       console.log("Generated mnemonic:" + generatedMnemonic)
-      return keyring.addFromMnemonic(generatedMnemonic)
+      return keyring.addFromMnemonic(generatedMnemonic, { name: userName })
     }
   }
 
   /**
-   * Mint tokens with different cases:
-   * 1. Account haven't tokens. This function has minted it.
-   * 2. Account have token. Then balance will be checked and if it less than the Amount, mint will be executed.
-   * 3. If the list of Assets is passed, than each Asset from list will be minted
-   * @param utils 
-   * @param api 
-   * @param sudoKeyPair 
-   * @param dexApi 
-   * @param currentAmount 
-   * @param assets 
+   * Mint all assets from KnownAssets on Amount to this.account or receiverKeyring if it passed 
+   * @param dexKeyPair Account with access to mint assets
+   * @param amount Amount for mint, keep decimal of asset in mind.
+   * @param receiver Receiver KeyringPair
+   * @param asset If passed mint only this asset
    */
-  public async mintTokens(sudoKeyPair: KeyringPair, currentAmount: number, assets?: Array<AccountAsset>): Promise<void>{
-    if (!assets){
-      for (const token of KnownAssets) {
-        const amount = currentAmount * 10**(token.decimals)
-        const accountAsset = this.accountAssets.find(i => i.symbol === token.symbol)
-        if (accountAsset){
-          const checkBalance = new FPNumber(amount.toString(), token.decimals)
-          if (+accountAsset.balance <= currentAmount) {
-            await this.customSubmitExtrinsic(this.api.tx.assets.mint(token.address, this.accountPair.address, "10000000"), sudoKeyPair, "Mint " + token.symbol)
-          }
-        } else {
-          await this.customSubmitExtrinsic(this.api.tx.assets.mint(token.address, token.address, amount.toString()), sudoKeyPair, "Mint " + token.symbol)
+  public async mintTokens(dexKeyPair: KeyringPair, amount: number, receiver?: KeyringPair, asset?: Asset): Promise<void>{
+    if (asset) {
+      //TODO Make it function more beautiful.
+      const amountString = new FPNumber(amount, asset.decimals).bnToString()
+      const receiverBalance = (await (this.api.rpc as any).assets.freeBalance(
+        (!receiver ? this.accountPair.address : receiver.address),
+        asset.address
+      )).unwrap().balance.toString()
+      if (+receiverBalance < +amountString){
+        try{
+          await this.customSubmitExtrinsic(this.api.tx.assets.mint(asset.address, this.accountPair.address, amountString), dexKeyPair, "Mint " + asset.symbol)
+        } catch (err){
+          console.log(err)
         }
+      } else {
+        console.log("=========================================================="+'\n'+
+        "User: "
+        +(!receiver ? this.accountPair.address : receiver.address)+
+        " have: "
+        +new FPNumber(receiverBalance, asset.decimals).toFixed()+
+        " of "+asset.symbol+ '\n'+
+        "==========================================================")
       }
     } else {
-      for (const token of assets){
-        const amount = currentAmount * 10**(token.decimals)
-        await this.customSubmitExtrinsic(this.api.tx.assets.mint(token.address, this.accountPair.address, amount.toString()), sudoKeyPair, "Mint " + token.symbol)
+      for (const token of KnownAssets) {
+        const amountString = new FPNumber(amount, token.decimals).bnToString()
+        const receiverBalance = (await (this.api.rpc as any).assets.freeBalance(
+          (!receiver ? this.accountPair.address : receiver.address),
+          token.address
+        )).unwrap().balance.toString()
+        if (+receiverBalance < +amountString){
+          await this.customSubmitExtrinsic(this.api.tx.assets.mint(token.address, this.accountPair.address, amountString), dexKeyPair, "Mint " + token.symbol)
+        } else {
+          console.log("=========================================================="+'\n'+
+          "User " +(!receiver ? this.accountPair.address : receiver.address)+ "have: " 
+          +new FPNumber(receiverBalance, token.decimals).toFixed()+
+          " of " +token.symbol+ '\n'+
+          "==========================================================")
+        }
       }
     }
   }
 
   /**
-   * Check all trading pair with KnownAssets and registered it if is not Enabled.
+   * Check if XOR trading pairs are created with tokens from KnownAssets if not, creates them
+   * @param sudoKeyPair account with access to create trading pairs
    */
-  public async createTraidingPairs(sudoKeyPair: KeyringPair): Promise<void>{
+  public async createTradingPairs(sudoKeyPair: KeyringPair): Promise<void>{
     const dexID = 0
     let enabled: boolean
-    for (const pair of KnownAssets) {
-      if (pair.symbol == "XOR")
-        continue
-      enabled = await (this.api.rpc as any).tradingPair.isPairEnabled(dexID, KnownAssets.get(KnownSymbols.XOR).address, pair.address)
-      if (enabled == false){
-        await this.customSubmitExtrinsic(this.api.tx.tradingPair.register(0, KnownAssets.get(KnownSymbols.XOR).address, pair.address), sudoKeyPair, "Enable Pair XOR - " + pair.symbol)
-
-        await this.customSubmitExtrinsic(this.api.tx.poolXyk.initializePool(0, KnownAssets.get(KnownSymbols.XOR).address, pair.address), sudoKeyPair, "Initialize Pool XOR - " + pair.symbol);
-        console.log("Pair of XOR - " + pair.symbol + "was registered!")
-      }
-    }
-    console.log("All traiding pair is registered!")
-  }
-
-  public async addLiqudity(sudoKeyPair: KeyringPair, firstTokenAmount: number, secondTokenAmount: number, assets?: Array<AccountAsset>): Promise<void>{
-    const ListAsset = await (this.api.rpc as any).assets.listAssetInfos()
-    // for (const xyk of ListAsset){
-    //   if (xyk.symbol == "XYKPOOL") {
-    //     //add object for make tokens pool
-    //     console.log("11")
-    //   }
-    // }
+    const xor = KnownAssets.get(KnownSymbols.XOR)
     for (const token of KnownAssets) {
-      const firstAsset = firstTokenAmount * 10**(token.decimals)
-      const secondAsset = secondTokenAmount * 10**(token.decimals)
-      if (token.symbol == "VAL"){
-        await this.customSubmitExtrinsic(this.api.tx.poolXyk.depositLiquidity(0, KnownAssets.get(KnownSymbols.XOR).address, token.address, "10000000000000", "10000000000000", "0", "0"), this.accountPair, "Add liquidity XOR - " + token.symbol)
-        // await this.addLiquidity(KnownAssets.get(KnownSymbols.XOR).address, token.address, 10, 10)
+      if (token.symbol == "XOR") {
+        continue
       }
-      // const totalSupply = await (this.api.rpc as any).assets.totalSupply()
-      // if (+totalSupply.unwrap().balance.toString() == 0) {
-      
-      // }
+      enabled = await (this.api.rpc as any).tradingPair.isPairEnabled(dexID, xor.address, token.address)
+      if (enabled == false){
+        await this.customSubmitExtrinsic(this.api.tx.tradingPair.register(0, xor.address, token.address), sudoKeyPair, "Enable Pair XOR - " + token.symbol)
+
+        await this.customSubmitExtrinsic(this.api.tx.poolXyk.initializePool(0, xor.address, token.address), sudoKeyPair, "Initialize Pool XOR - " + token.symbol);
+        console.log("Pair of XOR - " + token.symbol + "was registered!")
+      }
+    }
+    console.log("All trading pair is registered!")
+  }
+
+  /**
+   * Check balance of receiver, if it lower that amount send him from sender.
+   * @param receiver Account that receive
+   * @param amount Amount of token
+   * @param asset Asset to be sent
+   * @param sender Account that sends
+   */
+  public async sendTokenIfNeed(receiver: KeyringPair, amount: number, asset: Asset,  sender: KeyringPair): Promise<void> {
+    const amountString = new FPNumber(amount, asset.decimals).bnToString()
+    const receiverBalance = (await (this.api.rpc as any).assets.freeBalance(
+      receiver.address,
+      asset.address
+    )).unwrap().balance.toString()
+    if (+receiverBalance < +amountString){
+      await this.customSubmitExtrinsic(this.api.tx.assets.transfer(
+        asset.address,
+        receiver.address,
+        amountString
+      ), sender, "Transfer " +asset.symbol+ " amount: " +amount+ " from: " +sender.address+ " to: " +receiver.address)
+    } else {
+      console.log("=========================================================="+'\n'+
+      "User " +receiver.address+ "have: " 
+      +new FPNumber(receiverBalance, asset.decimals).toFixed()+
+      " of " +asset.symbol+ '\n'+
+      "==========================================================")
     }
   }
 
-  public str2ab(str): Uint8Array {
+  /**
+   * Checks the total liquidity of PoolTokens with XOR and the asset from KnownAssets, if it is empty makes a deposit.
+   * @param amount 
+   */
+  public async setLiquidity(amount: number): Promise<void>{
+    // const ListAsset = await (this.api.rpc as any).assets.listAssetInfos() //TODO make add liquidity for all assets
+    const xor = KnownAssets.get(KnownSymbols.XOR)
+    for (const token of KnownAssets) {
+      const amountToString = new FPNumber(amount, token.decimals).bnToString()
+      if (token.symbol == "XOR"){
+        continue
+      }
+      const totalLiquidity = await this.getLiquidityReserves(xor.address, token.address)
+      if (+totalLiquidity[0] == 0 || +totalLiquidity[1] == 0) {
+        await this.customSubmitExtrinsic(this.api.tx.poolXyk.depositLiquidity(
+          0,
+          xor.address,
+          token.address,
+          amountToString,
+          amountToString,
+          "0",
+          "0"
+        ), this.accountPair, "Add liquidity XOR - " + token.symbol)
+      }
+    }
+  }
+
+  public symbolToU8Array(str): Uint8Array {
     const buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
     const bufView = new Uint8Array(buf);
     for (let i = 0, strLen = str.length; i < strLen; i++) {
@@ -173,31 +223,38 @@ export class TestApi extends DexApi {
   }
 
   public async createAsset(sudoKeyPair: KeyringPair, asset: Asset): Promise<void> {
-    // const symbol = str2ab(asset.symbol)
+    // const symbol = symbolToU8Array(asset.symbol)
     // await utils.submitExtrinsic(api, api.tx.assets.register(), sudoKeyPair, "Register - " + asset.symbol)
   }
 
-  public async setupEnvironment(sudoSeed: string, testUserSeed: string): Promise<void>{
+  /**
+   * Checks the environment if it needs mint tokens, create trading pairs and add liquidity.
+   * @param sudoSeed 
+   * @param testUserSeed 
+   * @param dexSeed 
+   */
+  public async setupEnvironment(sudoSeed: string, testUserSeed: string, dexSeed: string): Promise<void>{
     console.log('Setup environment')
-
     //Some preparation things
+    const xor = KnownAssets.get(KnownSymbols.XOR)
+    const amountForDex = 10
+    const amountForMinting = 5000
+    const amountForLiquidity = 1000
     this.importAccount(testUserSeed,"TestUser","1")
     const sudoKeyPair = this.createKeyring('sr25519', 'SudoUser', sudoSeed)
+    const dexKeyPair = this.createKeyring('sr25519', 'DexUser', dexSeed)
     await this.getKnownAccountAssets()
-    // await dexApi.getAccountAsset(dexApi.accountPair.address)
 
+    //Check balance of DEX user if it small transfer from sudo
+    await this.sendTokenIfNeed(dexKeyPair, amountForDex, xor, sudoKeyPair)
     //Create assets
-    // await createAsset(utils, api, sudoKeyPair, KnownAssets.get(KnownSymbols.DOT))
-
+    // await createAsset(utils, api, sudoKeyPair, KnownAssets.get(KnownSymbols.DOT)) //TODO not implemented.
     //Mint tokens
-    await this.mintTokens(sudoKeyPair, 1000)
-
+    await this.mintTokens(dexKeyPair, amountForMinting)
     // Check and create trading pairs
-    await this.createTraidingPairs(sudoKeyPair)
-
+    await this.createTradingPairs(sudoKeyPair)
     // Add liquidity
-    await this.addLiqudity(sudoKeyPair, 100, 100)
-
+    await this.setLiquidity(amountForLiquidity)
     console.log("end")
     Promise.resolve()
   }
