827c72e8008ef37ec96c2ea84496c716226c99a1
Stefan Popov <stefanpopov@list.ru>
2021-02-02 17:13:06 +0400

    Update logger.ts

--- packages/util/src/logger.ts
+++ packages/util/src/logger.ts
@@ -1,5 +1,6 @@
 export const Messages = {
   connectWallet: 'You should connect wallet',
   xorIsRequired: 'You should have XOR token for pair creation',
-  endpointIsUndefined: 'You should set endpoint for connection'
+  endpointIsUndefined: 'You should set endpoint for connection',
+  pairAlreadyCreated: 'Token pair was already created'
 }
