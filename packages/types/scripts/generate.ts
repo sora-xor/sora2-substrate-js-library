30da7fb1333987099610684d82fe1094c0973809
Stefan Popov <stefanpopov@list.ru>
2021-05-17 15:05:38 +0400

    Add scripts for different environments

--- packages/types/scripts/generate.ts
+++ packages/types/scripts/generate.ts
@@ -10,7 +10,6 @@ import {
 } from '@polkadot/typegen/generate';
 import { registerDefinitions } from '@polkadot/typegen/util'
 import generateMobx from '@open-web3/api-mobx/scripts/mobx'
-import metaHex from '../src/metadata/latest'
 
 import * as defaultDefinitions from '@polkadot/types/interfaces/definitions'
 
@@ -18,20 +17,22 @@ import * as ormlDefinitions from '@open-web3/orml-types/interfaces/definitions'
 
 import * as soraDefinitions from '../src/interfaces/definitions'
 
-function filterModules(names: string[], defs: any): string {
-  const registry = new TypeRegistry();
-  registerDefinitions(registry, defs);
-  const metadata = new Metadata(registry, metaHex);
+function filterModules(names: string[], defs: any, metaHex: any): string {
+  const registry = new TypeRegistry()
+  registerDefinitions(registry, defs)
+  console.log(metaHex)
+
+  const metadata = new Metadata(registry, metaHex)
 
   // populate registry workaround
-  metadata.asLatest.toJSON();
+  metadata.asLatest.toJSON()
 
-  const filtered = metadata.toJSON() as any;
+  const filtered = metadata.toJSON() as any
 
-  filtered.metadata.v12.modules = filtered.metadata.v12.modules.filter(({ name }: any) => names.includes(name));
-  console.log("Enabled modules:\n" + filtered.metadata.v12.modules.map(({ name }: any) => name).join("\n") + "\n");
+  filtered.metadata.v12.modules = filtered.metadata.v12.modules.filter(({ name }: any) => names.includes(name))
+  console.log('Enabled modules:\n' + filtered.metadata.v12.modules.map(({ name }: any) => name).join('\n') + '\n')
 
-  return new Metadata(registry, filtered).toHex();
+  return new Metadata(registry, filtered).toHex()
 }
 
 const { runtime, ...substrateDefinitions } = defaultDefinitions
@@ -44,45 +45,50 @@ const definitions = {
   '@sora-substrate/types/interfaces': soraDefinitions
 } as any
 
-const metadata = filterModules(
-  [
-    // // 'System', // guaranteed conflict with extensions
-    'Timestamp',
-    'Balances',
-    // 'Sudo',
-    'RandomnessCollectiveFlip',
-    // 'ParachainUpgrade',
-    'MessageBroker',
-    'TransactionPayment',
-    // 'ParachainInfo',
-    'Permissions',
-    'TokenDealer',
-    'TemplateModule',
-    'ReferralSystem',
-    'XorFee',
-    'Tokens',
-    'Currencies',
-    'TradingPair',
-    // 'Assets',
-    'DEXManager',
-    'BondingCurvePool',
-    'Technical',
-    'LiquidityProxy',
-    'MockLiquiditySource',
-    'MockLiquiditySource2',
-    'MockLiquiditySource3',
-    'MockLiquiditySource4',
-    'DEXAPI'
-  ],
-  definitions
-);
+const env = process.argv[2]
+import(`../src/metadata${env ? '/' + env : ''}/latest`).then(meta => {
+  const metaHex = meta.default ?? meta
+  const metadata = filterModules(
+    [
+      // // 'System', // guaranteed conflict with extensions
+      'Timestamp',
+      'Balances',
+      // 'Sudo',
+      'RandomnessCollectiveFlip',
+      // 'ParachainUpgrade',
+      'MessageBroker',
+      'TransactionPayment',
+      // 'ParachainInfo',
+      'Permissions',
+      'TokenDealer',
+      'TemplateModule',
+      'ReferralSystem',
+      'XorFee',
+      'Tokens',
+      'Currencies',
+      'TradingPair',
+      // 'Assets',
+      'DEXManager',
+      'BondingCurvePool',
+      'Technical',
+      'LiquidityProxy',
+      'MockLiquiditySource',
+      'MockLiquiditySource2',
+      'MockLiquiditySource3',
+      'MockLiquiditySource4',
+      'DEXAPI'
+    ],
+    definitions,
+    metaHex
+  )
 
-generateTsDef(definitions, 'packages/types/src/interfaces', '@sora-substrate/types/interfaces')
-generateInterfaceTypes(definitions, 'packages/types/src/interfaces/augment-types.ts')
-generateDefaultConsts('packages/types/src/interfaces/augment-api-consts.ts', metadata, definitions)
+  generateTsDef(definitions, 'packages/types/src/interfaces', '@sora-substrate/types/interfaces')
+  generateInterfaceTypes(definitions, 'packages/types/src/interfaces/augment-types.ts')
+  generateDefaultConsts('packages/types/src/interfaces/augment-api-consts.ts', metadata, definitions)
 
-generateDefaultTx('packages/types/src/interfaces/augment-api-tx.ts', metadata, definitions)
-generateDefaultQuery('packages/types/src/interfaces/augment-api-query.ts', metadata, definitions)
-generateDefaultRpc('packages/types/src/interfaces/augment-api-rpc.ts', definitions)
+  generateDefaultTx('packages/types/src/interfaces/augment-api-tx.ts', metadata, definitions)
+  generateDefaultQuery('packages/types/src/interfaces/augment-api-query.ts', metadata, definitions)
+  generateDefaultRpc('packages/types/src/interfaces/augment-api-rpc.ts', definitions)
 
-generateMobx('packages/types/src/interfaces/augment-api-mobx.ts', metaHex, definitions)
+  generateMobx('packages/types/src/interfaces/augment-api-mobx.ts', metaHex, definitions)
+})
--- packages/types/scripts/pullMeta.ts
+++ packages/types/scripts/pullMeta.ts
@@ -1,17 +1,40 @@
 import fs from 'fs'
 import { w3cwebsocket as WebSocket } from 'websocket'
 
-function pullMetadata(api_endpoint: string, target_file: string) {
-  console.log(`Pulling from ${api_endpoint}`);
-  const ws = new WebSocket(api_endpoint);
-  ws.onopen = (): void => {
-    ws.send('{"id":"1","jsonrpc":"2.0","method":"state_getMetadata","params":[]}')
-  }
-  ws.onmessage = (msg: any): void => {
-    const metadata = JSON.parse(msg.data).result
-    fs.writeFileSync(target_file, `export default '${metadata}'`)
-    console.log('Done')
-    process.exit(0)
-  }
+export enum SORA_ENV {
+  stage = 'wss://ws.stage.sora2.soramitsu.co.jp',
+  prod = 'wss://ws.sora2.soramitsu.co.jp',
+  // test = 'wss://ws.framenode-1.s1.tst.sora2.soramitsu.co.jp',
+  dev = 'wss://ws.framenode-1.s1.dev.sora2.soramitsu.co.jp',
 }
-pullMetadata('ws://localhost:9944', 'packages/types/src/metadata/latest.ts')
+
+async function pullMetadata(apiEndpoint: string, targetFile: string) {
+  console.log(`Pulling from ${apiEndpoint}`)
+  const ws = new WebSocket(apiEndpoint)
+
+  await new Promise<void>(resolve => {
+    ws.onopen = (): void => {
+      ws.send('{"id":"1","jsonrpc":"2.0","method":"state_getMetadata","params":[]}')
+      resolve()
+    }
+  })
+  await new Promise<void>(resolve => {
+    ws.onmessage = (msg: any): void => {
+      const metadata = JSON.parse(msg.data).result
+      fs.writeFileSync(targetFile, `export default '${metadata}'`)
+      console.log(`${apiEndpoint}: Done!`)
+      resolve()
+    }
+  })
+  ws.close()
+}
+
+const promises = [] as Array<Promise<void>>
+if (process.argv[2] === 'all') {
+  Object.entries(SORA_ENV).forEach(([name, endpoint]) => {
+    promises.push(pullMetadata(endpoint, `packages/types/src/metadata/${name}/latest.ts`))
+  })
+} else {
+  promises.push(pullMetadata('ws://localhost:9944', 'packages/types/src/metadata/latest.ts'))
+}
+Promise.all(promises).then(() => process.exit(0))
