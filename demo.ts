ae264a24b7453fda1beea392d030552dda6a7e1c
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-06-03 16:19:15 +0300

    revert demo.ts

--- demo.ts
+++ demo.ts
@@ -8,7 +8,7 @@ async function demo(): Promise<void> {
   console.log('INITIALIZING API');
 
   // NOTE: replace to use relevant endpoint: 'ws://localhost:9944/' for local, wss://ws.address.of.chain:9944/ for testnet
-  const provider = new WsProvider('wss://ws.framenode-1.s1.dev.sora2.soramitsu.co.jp/');
+  const provider = new WsProvider('ws://localhost:9944/');
   const api = new ApiPromise(options({ provider }));
   await api.isReady;
 
@@ -29,12 +29,6 @@ async function demo(): Promise<void> {
   const VALAssetId = api.createType('AssetId', '0x0200040000000000000000000000000000000000000000000000000000000000');
   const PSWAPAssetId = api.createType('AssetId', '0x0200050000000000000000000000000000000000000000000000000000000000');
   const DAIAssetId = api.createType('AssetId', '0x0200060000000000000000000000000000000000000000000000000000000000');
-
-  const rewards = await api.query.vestedRewards.rewards('cnW4qGRcbepL9bZRw7DS2RdYgRyiW2zsKoq1cdZVZQpRL2ftG')
-
-  console.log(rewards)
-
-  return
 
   // register pair
   await submitExtrinsic(api, api.tx.tradingPair.register(0, XORAssetId, PSWAPAssetId), root, "Enable Pair XOR-DOT");
