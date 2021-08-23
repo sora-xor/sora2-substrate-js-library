2631b22947eaf0d4bb658e76815e4b7461a236eb
Stefan Popov <stefanpopov@list.ru>
2021-08-23 08:48:58 +0400

    Fix

--- scripts/verifyPoolAccountGeneration.ts
+++ scripts/verifyPoolAccountGeneration.ts
@@ -1,8 +1,7 @@
 import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
 import { options } from '@sora-substrate/api';
-import { Keyring } from '@polkadot/api';
-import { poolAccountIdFromAssetPair } from '@sora-substrate/util/poolAccount';
+import { poolAccountIdFromAssetPair } from '@sora-substrate/util';
 
 async function main(): Promise<void> {
   const provider = new WsProvider('wss://ws.mof.sora.org');
