a7cf22508fd0796a169b8a91e82638a46533b514
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-09-29 15:36:10 +0300

    improve amounts for lp history items

--- packages/util/src/api.ts
+++ packages/util/src/api.ts
@@ -1218,7 +1218,10 @@ export class Api extends BaseApi {
         desiredB.sub(desiredB.mul(slippage)).toCodecString()
       ],
       firstAsset,
-      secondAsset
+      secondAsset,
+      // amountA, amountB - without slippage (for initial history)
+      amountA: desiredA.toString(),
+      amountB: desiredB.toString()
     }
   }
 
@@ -1259,7 +1262,8 @@ export class Api extends BaseApi {
         assetAddress: params.firstAsset.address,
         symbol2: params.secondAsset.symbol,
         asset2Address: params.secondAsset.address,
-        amount: `${desiredMarker}`
+        amount: params.amountA,
+        amount2: params.amountB
       }
     )
   }
