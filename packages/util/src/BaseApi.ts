06a01b04b5e751de3acbb6466a4ee23389432c6e
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2021-12-20 14:08:21 +0300

    Remove decimals from interface

--- packages/util/src/BaseApi.ts
+++ packages/util/src/BaseApi.ts
@@ -552,7 +552,6 @@ export interface History {
   amount2?: string;
   symbol2?: string;
   asset2Address?: string;
-  decimals?: number;
   decimals2?: number;
   startTime?: number;
   endTime?: number;
