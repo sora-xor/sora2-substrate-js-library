b5b3e9ce86c21bdf089f2d47c211a363413ae606
Danijel Radulović <master.daca09@gmail.com>
2021-12-31 01:04:58 +0100

    Add Ceres staking and Liquidity Locker types

---
+++ packages/type-definitions/src/ceresLiquidityLocker.ts
@@ -0,0 +1,11 @@
+export default {
+    rpc: {},
+    types: {
+        LockInfo: {
+            pool_tokens: "Balance",
+            unlocking_block: "BlockNumber",
+            asset_a: "AssetId",
+            asset_b: "AssetId",
+        },
+    }
+}
