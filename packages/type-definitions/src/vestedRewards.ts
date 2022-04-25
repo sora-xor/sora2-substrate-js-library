a8778069bfa3f3db5c4183befac81ec27f3abfa8
Ales Tsurko <ales.tsurko@gmail.com>
2022-04-25 13:22:36 +0300

    Add crowdloan lease period types definitions (#378)
    * Add crowdloan lease period types definitions
    
    * up version to 1.8.25
    
    Co-authored-by: Nikita-Polyakov <polyakov@soramitsu.co.jp>

--- packages/type-definitions/src/vestedRewards.ts
+++ packages/type-definitions/src/vestedRewards.ts
@@ -19,10 +19,26 @@ export default {
       ],
       type: 'Option<BalanceInfo>',
     },
+    crowdloanLease: {
+      description: 'Get crowdloan rewards lease period info.',
+      params: [
+        {
+          name: 'at',
+          type: 'BlockHash',
+          isOptional: true,
+        },
+      ],
+      type: 'CrowdloanLease',
+    },
   },
   types: {
     BalanceInfo: {
       balance: 'Balance',
     },
+    CrowdloanLease: {
+      start_block: 'String',
+      total_days: 'String',
+      blocks_per_day: 'String',
+    }
   },
 };
