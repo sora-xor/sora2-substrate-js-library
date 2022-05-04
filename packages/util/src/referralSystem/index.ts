3b1c41712baba4a797b367d53e6cc2c59ddfad5a
alexnatalia <alekseenko@soramitsu.co.jp>
2022-05-04 08:15:35 +0100

    Updated due to PR comments.

--- packages/util/src/referralSystem/index.ts
+++ packages/util/src/referralSystem/index.ts
@@ -34,7 +34,7 @@ export class ReferralSystemModule {
    * @param invitedUserId address of invited account
    */
   public subscribeOnReferrer(invitedUserId: string): Observable<string> {
-    return this.root.apiRx.query.referrals.referrers(invitedUserId) as unknown as Observable<string>;
+    return this.root.apiRx.query.referrals.referrers(invitedUserId).pipe(map((data) => data.toString()));
   }
 
   /**
