fed78bde1f8a4ad583b96145f49e89d389d00e67
Stefan Popov <stefanpopov@list.ru>
2022-05-12 15:40:17 +0300

    Fix test env types (#396)
    * Update types
    
    * Bump version

--- packages/types/scripts/consts.ts
+++ packages/types/scripts/consts.ts
@@ -1,6 +1,6 @@
 export enum SORA_ENV {
   prod = 'wss://mof3.sora.org',
   stage = 'wss://ws.stage.sora2.soramitsu.co.jp',
-  test = 'wss://ws.framenode-5.a1.tst.sora2.soramitsu.co.jp',
+  test = 'wss://ws.framenode-2.v1.tst.sora2.soramitsu.co.jp',
   dev = 'wss://ws.framenode-3.s3.dev.sora2.soramitsu.co.jp',
 }
