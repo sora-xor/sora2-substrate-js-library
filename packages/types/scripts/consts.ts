b4922e9ca0024c6d4ce85d8d4fd072b87cef9362
Stefan Popov <stefanpopov@list.ru>
2022-06-26 23:04:38 +0400

    Update export types dev script

--- packages/types/scripts/consts.ts
+++ packages/types/scripts/consts.ts
@@ -2,5 +2,6 @@ export enum SORA_ENV {
   prod = 'wss://ws.sora2.soramitsu.co.jp',
   stage = 'wss://ws.stage.sora2.soramitsu.co.jp',
   test = 'wss://ws.framenode-2.v1.tst.sora2.soramitsu.co.jp',
-  dev = 'wss://ws.framenode-3.s3.dev.sora2.soramitsu.co.jp',
+  /** Substrate 4 dev node */
+  dev = 'wss://ws.framenode-1.sub4.dev.sora2.soramitsu.co.jp',
 }
