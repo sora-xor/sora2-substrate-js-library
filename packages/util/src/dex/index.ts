0f835fc687e1be8603255081f66e0fd13181706c
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2022-11-09 15:29:32 +0300

    add isPublic attr to dex

--- packages/util/src/dex/index.ts
+++ packages/util/src/dex/index.ts
@@ -10,7 +10,11 @@ export class DexModule {
   public static defaultDexId = DexId.XOR;
   public static defaultBaseAssetId = XOR.address;
 
-  public dexList: DexInfo[] = [];
+  private _dexes: DexInfo[] = [];
+
+  public get dexList(): DexInfo[] {
+    return this._dexes.filter((dex) => !!dex.isPublic);
+  }
 
   get baseAssetsIds(): string[] {
     return this.dexList.map((item) => item.baseAssetId);
@@ -19,19 +23,20 @@ export class DexModule {
   public async updateList(): Promise<void> {
     const data = await this.root.api.query.dexManager.dexInfos.entries();
 
-    this.dexList = data.map(([key, codec]) => {
+    this._dexes = data.map(([key, codec]) => {
       const dexId = key.args[0].toNumber();
       const baseAssetId = codec.value.baseAssetId.code.toString();
+      const isPublic = codec.value.isPublic.isTrue;
 
-      return { dexId, baseAssetId };
+      return { dexId, baseAssetId, isPublic };
     });
   }
 
   public getDexId(baseAssetId: string): number {
-    return this.dexList.find((item) => item.baseAssetId === baseAssetId)?.dexId ?? DexModule.defaultDexId;
+    return this.dexList.find((dex) => dex.baseAssetId === baseAssetId)?.dexId ?? DexModule.defaultDexId;
   }
 
   public getBaseAssetId(dexId: number): string {
-    return this.dexList.find((item) => item.dexId === dexId)?.baseAssetId ?? DexModule.defaultBaseAssetId;
+    return this.dexList.find((dex) => dex.dexId === dexId)?.baseAssetId ?? DexModule.defaultBaseAssetId;
   }
 }
