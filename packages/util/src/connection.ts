a366a48b1ecf26816c79374783ebf2aaa6cd7232
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2023-02-14 12:17:07 +0300

    refactoring connection class (#517)
    * refactoring connection class
    
    * add warn message

--- packages/util/src/connection.ts
+++ packages/util/src/connection.ts
@@ -2,25 +2,55 @@ import { assert } from '@polkadot/util';
 import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
 import { options } from '@sora-substrate/api';
-import type { ProviderInterfaceEmitted, ProviderInterfaceEmitCb } from '@polkadot/rpc-provider/types';
+import type { ProviderInterfaceEmitCb } from '@polkadot/rpc-provider/types';
 import type { ApiInterfaceEvents } from '@polkadot/api/types';
 
 import { Messages } from './logger';
+
+type ConnectionEventListener = [ApiInterfaceEvents, ProviderInterfaceEmitCb];
 
 export interface ConnectionRunOptions {
   once?: boolean;
   timeout?: number;
   autoConnectMs?: number;
-  eventListeners?: Array<[ProviderInterfaceEmitted, ProviderInterfaceEmitCb]>;
-  /** Just for External clients for now */
-  apiEventListeners?: Array<[ApiInterfaceEvents, ProviderInterfaceEmitCb]>;
+  providerTimeout?: number; // second between disconnect and "disconnect" event emitting
+  eventListeners?: ConnectionEventListener[];
 }
+
+const addEventListeners = (api: ApiPromise, eventListeners: ConnectionEventListener[]): void => {
+  eventListeners.forEach(([eventName, eventHandler]) => api.on(eventName, eventHandler));
+};
+
+const removeEventListeners = (api: ApiPromise, eventListeners: ConnectionEventListener[]): void => {
+  eventListeners.forEach(([eventName, eventHandler]) => api.off(eventName, eventHandler));
+};
+
+const disconnectApi = async (api: ApiPromise, eventListeners: ConnectionEventListener[]): Promise<void> => {
+  if (!api) return;
+
+  removeEventListeners(api, eventListeners);
+  // close the connection manually
+  if (api.isConnected) {
+    try {
+      await api.disconnect();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+const createConnectionTimeout = (timeout: number): Promise<void> => {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject('Connection Timeout'), timeout);
+  });
+};
 
 class Connection {
   public api: ApiPromise | null = null;
   public endpoint = '';
   public loading = false;
-  public eventHandlers: Array<Function> = [];
+
+  private eventListeners: Array<[ApiInterfaceEvents, ProviderInterfaceEmitCb]> = [];
 
   private async withLoading(func: Function): Promise<any> {
     this.loading = true;
@@ -34,91 +64,55 @@ class Connection {
   }
 
   private async run(endpoint: string, runOptions?: ConnectionRunOptions): Promise<void> {
-    let connectionTimeout: any;
     const {
       once = false,
       timeout = 0,
       autoConnectMs = 5000,
+      providerTimeout = 0,
       eventListeners = [],
-      apiEventListeners = [],
     } = runOptions || {};
 
-    const prevEndpoint = this.endpoint;
-    this.endpoint = endpoint;
-
     const providerAutoConnectMs = once ? 0 : autoConnectMs;
-    const provider = new WsProvider(endpoint, providerAutoConnectMs);
-    // TODO: set it to disable all logs: noInitWarn
-    const api = new ApiPromise(options({ provider /* , noInitWarn: true */ }));
     const apiConnectionPromise = once ? 'isReadyOrError' : 'isReady';
 
-    // because this.endpoint can be overwritten by the next run call, which is faster
-    const connectionEndpointIsStable = () => this.endpoint === endpoint;
-    const runConnectionTimeout = (): Promise<void> => {
-      if (!timeout) return Promise.resolve();
-
-      return new Promise((_, reject) => {
-        connectionTimeout = setTimeout(() => reject('Connection Timeout'), timeout);
-      });
-    };
+    const provider = new WsProvider(endpoint, providerAutoConnectMs, undefined, providerTimeout);
+    const api = new ApiPromise(options({ provider, noInitWarn: true }));
 
     const connectionRequests: Array<Promise<any>> = [api[apiConnectionPromise]];
 
-    if (timeout) connectionRequests.push(runConnectionTimeout());
+    if (timeout) connectionRequests.push(createConnectionTimeout(timeout));
 
     try {
-      if (apiEventListeners.length > 0) {
-        apiEventListeners.map(([eventName, callback]) => api.on(eventName, callback));
-      }
-      this.api = api;
+      addEventListeners(api, eventListeners);
+
       // we should manually call connect fn without autoConnectMs
       if (!providerAutoConnectMs) {
-        this.api.connect();
+        api.connect();
       }
 
       await Promise.race(connectionRequests);
 
       // check race condition case: another call of 'run' was faster
-      if (!connectionEndpointIsStable()) {
-        this.api.disconnect();
-        return;
-      }
-
-      // add new event handlers
-      if (eventListeners.length > 0) {
-        this.eventHandlers = eventListeners.map(([eventName, callback]) => provider.on(eventName, callback));
+      if (this.opened) {
+        console.warn(`[Connection]: Close previous connection ${this.endpoint}, before estabilishing new one`);
+        console.warn(`[Connection]: Connection interrupted: ${endpoint}`);
+        disconnectApi(api, eventListeners);
+      } else {
+        this.api = api;
+        this.endpoint = endpoint;
+        this.eventListeners = eventListeners;
       }
     } catch (error) {
-      provider.disconnect();
-      if (connectionEndpointIsStable()) {
-        this.endpoint = prevEndpoint;
-        this.api = null;
-      }
+      disconnectApi(api, eventListeners);
       throw error;
-    } finally {
-      clearTimeout(connectionTimeout);
     }
   }
 
-  public unsubscribeEventHandlers(): void {
-    if (this.eventHandlers.length > 0) {
-      this.eventHandlers.forEach((unsubscribeFn) => unsubscribeFn());
-      this.eventHandlers = [];
-    }
-  }
-
-  private async stop(isConnected = true): Promise<void> {
-    this.unsubscribeEventHandlers();
-    // close the connection manually
-    if (isConnected && this.api) {
-      try {
-        await this.api.disconnect();
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  private async stop(): Promise<void> {
+    await disconnectApi(this.api, this.eventListeners);
     this.api = null;
     this.endpoint = '';
+    this.eventListeners = [];
   }
 
   public get opened(): boolean {
@@ -137,22 +131,9 @@ class Connection {
 
   /**
    * Close connection
-   * @param isConnected is current connection stable (node is connected)
    */
-  public async close(isConnected = true): Promise<void> {
-    await this.withLoading(async () => await this.stop(isConnected));
-  }
-
-  /**
-   * Close connection and open to new endpoint
-   * @param endpoint address of node
-   * @param options
-   */
-  public async restart(endpoint: string, options?: ConnectionRunOptions): Promise<void> {
-    await this.withLoading(async () => {
-      await this.stop();
-      await this.run(endpoint, options);
-    });
+  public async close(): Promise<void> {
+    await this.withLoading(async () => await this.stop());
   }
 }
 
