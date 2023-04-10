34870a310f237afa12d423b8af5ff1c9dca9b783
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-04-10 21:11:36 +0200

    Upgrade packages

--- scripts/examples/util.ts
+++ scripts/examples/util.ts
@@ -6,10 +6,11 @@ export async function delay(ms = 40000): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function connectAndImportAccount(env = SORA_ENV.stage): Promise<void> {
+async function connectAndImportAccount(env = SORA_ENV.dev): Promise<void> {
   await connection.open(env);
   console.info('Connected: ' + env);
   await api.initialize();
+  await api.calcStaticNetworkFees();
   // salon muscle select culture inform pen typical object fox fruit culture civil
   api.importAccount('street firm worth record skin taste legend lobster magnet stove drive side', 'name', 'pass');
 }
