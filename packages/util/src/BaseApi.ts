7a08d45eccbbbafebb672e56cab9ff31317a690f
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2023-05-17 09:45:57 +0300

    add try catch to getEmptyExtrinsic

--- packages/util/src/BaseApi.ts
+++ packages/util/src/BaseApi.ts
@@ -507,77 +507,81 @@ export class BaseApi<T = void> implements ISubmitExtrinsic<T> {
    * @param operation
    */
   private getEmptyExtrinsic(operation: Operation): SubmittableExtrinsic<'promise'> | null {
-    switch (operation) {
-      case Operation.AddLiquidity:
-        return this.api.tx.poolXYK.depositLiquidity(DexId.XOR, '', '', '0', '0', '0', '0');
-      case Operation.CreatePair:
-        return this.api.tx.utility.batchAll([
-          this.api.tx.tradingPair.register(DexId.XOR, '', ''),
-          this.api.tx.poolXYK.initializePool(DexId.XOR, '', ''),
-          this.api.tx.poolXYK.depositLiquidity(DexId.XOR, '', '', '0', '0', '0', '0'),
-        ]);
-      case Operation.EthBridgeIncoming:
-      case Operation.EvmIncoming:
-        return null;
-      case Operation.EthBridgeOutgoing:
-        return this.api.tx.ethBridge.transferToSidechain('', '', '0', 0);
-      case Operation.EvmOutgoing:
-        return this.api.tx.evmBridgeProxy.burn({ EVM: 1 }, '', { EVM: '' }, '0');
-      case Operation.RegisterAsset:
-        return this.api.tx.assets.register('', '', '0', false, false, null, null);
-      case Operation.RemoveLiquidity:
-        return this.api.tx.poolXYK.withdrawLiquidity(DexId.XOR, '', '', '0', '0', '0');
-      case Operation.Swap:
-        return this.api.tx.liquidityProxy.swap(
-          DexId.XOR,
-          '',
-          '',
-          { WithDesiredInput: { desiredAmountIn: '0', minAmountOut: '0' } },
-          [],
-          'Disabled'
-        );
-      case Operation.SwapAndSend:
-        return this.api.tx.liquidityProxy.swapTransfer(
-          '',
-          DexId.XOR,
-          '',
-          '',
-          { WithDesiredInput: { desiredAmountIn: '0', minAmountOut: '0' } },
-          [],
-          'Disabled'
-        );
-      case Operation.Transfer:
-        return this.api.tx.assets.transfer('', '', '0');
-      case Operation.ClaimVestedRewards:
-        return this.api.tx.vestedRewards.claimRewards();
-      case Operation.ClaimCrowdloanRewards:
-        return this.api.tx.vestedRewards.claimCrowdloanRewards(XOR.address);
-      case Operation.ClaimLiquidityProvisionRewards:
-        return this.api.tx.pswapDistribution.claimIncentive();
-      case Operation.ClaimExternalRewards:
-        return this.api.tx.rewards.claim(
-          '0xa8811ca9a2f65a4e21bd82a1e121f2a7f0f94006d0d4bcacf50016aef0b67765692bb7a06367365f13a521ec129c260451a682e658048729ff514e77e4cdffab1b'
-        ); // signature mock
-      case Operation.ReferralReserveXor:
-        return this.api.tx.referrals.reserve('0');
-      case Operation.ReferralUnreserveXor:
-        return this.api.tx.referrals.unreserve('0');
-      case Operation.ReferralSetInvitedUser:
-        return this.api.tx.referrals.setReferrer('');
-      case Operation.DemeterFarmingDepositLiquidity:
-        return this.api.tx.demeterFarmingPlatform.deposit(XOR.address, XOR.address, XOR.address, true, 0);
-      case Operation.DemeterFarmingWithdrawLiquidity:
-        return this.api.tx.demeterFarmingPlatform.withdraw(XOR.address, XOR.address, XOR.address, 0, true);
-      case Operation.DemeterFarmingStakeToken:
-        return this.api.tx.demeterFarmingPlatform.deposit(XOR.address, XOR.address, XOR.address, false, 0);
-      case Operation.DemeterFarmingUnstakeToken:
-        return this.api.tx.demeterFarmingPlatform.withdraw(XOR.address, XOR.address, XOR.address, 0, false);
-      case Operation.DemeterFarmingGetRewards:
-        return this.api.tx.demeterFarmingPlatform.getRewards(XOR.address, XOR.address, XOR.address, true);
-      case Operation.CeresLiquidityLockerLockLiquidity:
-        return this.api.tx.ceresLiquidityLocker.lockLiquidity(XOR.address, XOR.address, 0, 100, false);
-      default:
-        return null;
+    try {
+      switch (operation) {
+        case Operation.AddLiquidity:
+          return this.api.tx.poolXYK.depositLiquidity(DexId.XOR, '', '', '0', '0', '0', '0');
+        case Operation.CreatePair:
+          return this.api.tx.utility.batchAll([
+            this.api.tx.tradingPair.register(DexId.XOR, '', ''),
+            this.api.tx.poolXYK.initializePool(DexId.XOR, '', ''),
+            this.api.tx.poolXYK.depositLiquidity(DexId.XOR, '', '', '0', '0', '0', '0'),
+          ]);
+        case Operation.EthBridgeIncoming:
+        case Operation.EvmIncoming:
+          return null;
+        case Operation.EthBridgeOutgoing:
+          return this.api.tx.ethBridge.transferToSidechain('', '', '0', 0);
+        case Operation.EvmOutgoing:
+          return this.api.tx.evmBridgeProxy.burn({ EVM: 1 }, '', { EVM: '' }, '0');
+        case Operation.RegisterAsset:
+          return this.api.tx.assets.register('', '', '0', false, false, null, null);
+        case Operation.RemoveLiquidity:
+          return this.api.tx.poolXYK.withdrawLiquidity(DexId.XOR, '', '', '0', '0', '0');
+        case Operation.Swap:
+          return this.api.tx.liquidityProxy.swap(
+            DexId.XOR,
+            '',
+            '',
+            { WithDesiredInput: { desiredAmountIn: '0', minAmountOut: '0' } },
+            [],
+            'Disabled'
+          );
+        case Operation.SwapAndSend:
+          return this.api.tx.liquidityProxy.swapTransfer(
+            '',
+            DexId.XOR,
+            '',
+            '',
+            { WithDesiredInput: { desiredAmountIn: '0', minAmountOut: '0' } },
+            [],
+            'Disabled'
+          );
+        case Operation.Transfer:
+          return this.api.tx.assets.transfer('', '', '0');
+        case Operation.ClaimVestedRewards:
+          return this.api.tx.vestedRewards.claimRewards();
+        case Operation.ClaimCrowdloanRewards:
+          return this.api.tx.vestedRewards.claimCrowdloanRewards(XOR.address);
+        case Operation.ClaimLiquidityProvisionRewards:
+          return this.api.tx.pswapDistribution.claimIncentive();
+        case Operation.ClaimExternalRewards:
+          return this.api.tx.rewards.claim(
+            '0xa8811ca9a2f65a4e21bd82a1e121f2a7f0f94006d0d4bcacf50016aef0b67765692bb7a06367365f13a521ec129c260451a682e658048729ff514e77e4cdffab1b'
+          ); // signature mock
+        case Operation.ReferralReserveXor:
+          return this.api.tx.referrals.reserve('0');
+        case Operation.ReferralUnreserveXor:
+          return this.api.tx.referrals.unreserve('0');
+        case Operation.ReferralSetInvitedUser:
+          return this.api.tx.referrals.setReferrer('');
+        case Operation.DemeterFarmingDepositLiquidity:
+          return this.api.tx.demeterFarmingPlatform.deposit(XOR.address, XOR.address, XOR.address, true, 0);
+        case Operation.DemeterFarmingWithdrawLiquidity:
+          return this.api.tx.demeterFarmingPlatform.withdraw(XOR.address, XOR.address, XOR.address, 0, true);
+        case Operation.DemeterFarmingStakeToken:
+          return this.api.tx.demeterFarmingPlatform.deposit(XOR.address, XOR.address, XOR.address, false, 0);
+        case Operation.DemeterFarmingUnstakeToken:
+          return this.api.tx.demeterFarmingPlatform.withdraw(XOR.address, XOR.address, XOR.address, 0, false);
+        case Operation.DemeterFarmingGetRewards:
+          return this.api.tx.demeterFarmingPlatform.getRewards(XOR.address, XOR.address, XOR.address, true);
+        case Operation.CeresLiquidityLockerLockLiquidity:
+          return this.api.tx.ceresLiquidityLocker.lockLiquidity(XOR.address, XOR.address, 0, 100, false);
+        default:
+          return null;
+      }
+    } catch {
+      return null;
     }
   }
 
