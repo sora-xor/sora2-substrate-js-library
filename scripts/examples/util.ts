b35fcf8df549110f4e40a3faf04a651a16f3e217
Stefan Popov <stefanpopov@list.ru>
2023-09-04 13:03:17 +0200

    Feature refactor system module (#603)
    * Refactor system module
    
    * Update test util
    
    * Add hasEnoughXorForFee for referrer
    
    * Bump version
    
    ---------
    
    Co-authored-by: Stefan Popov <17688105+stefashkaa@users.noreply.github.com>

--- scripts/examples/util.ts
+++ scripts/examples/util.ts
@@ -3,25 +3,28 @@ import { connection } from '@sora-substrate/connection';
 import { SORA_ENV } from '@sora-substrate/types/scripts/consts';
 // import { mnemonicGenerate } from '@polkadot/util-crypto'; // TODO: use it within the faucet
 
+const TST_MNEMONIC = 'street firm worth record skin taste legend lobster magnet stove drive side';
+
 export async function delay(ms = 40000): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function connectAndImportAccount(env = SORA_ENV.dev): Promise<void> {
+async function connectAndImportAccount(env = SORA_ENV.dev, mnemonic?: string): Promise<void> {
   await connection.open(env);
   console.info('Connected: ' + env);
   await api.initialize();
   await api.calcStaticNetworkFees();
   // salon muscle select culture inform pen typical object fox fruit culture civil
-  api.importAccount('street firm worth record skin taste legend lobster magnet stove drive side', 'name', 'pass');
+  api.importAccount(mnemonic ?? TST_MNEMONIC, 'name', 'pass');
 }
 
 async function disconnect(): Promise<void> {
+  api.logout();
   await connection.close();
 }
 
-export async function withConnectedAccount(fn): Promise<void> {
-  await connectAndImportAccount();
+export async function withConnectedAccount(fn: Function, env = SORA_ENV.dev, mnemonic?: string): Promise<void> {
+  await connectAndImportAccount(env, mnemonic);
   await fn();
   await disconnect();
 }
