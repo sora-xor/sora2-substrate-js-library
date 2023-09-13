b9e910a80db1c788362da76180910e1e4723d565
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-09-13 09:26:30 +0200

    Add swap & fees performance examples

--- scripts/examples/fees.ts
+++ scripts/examples/fees.ts
@@ -1,6 +1,6 @@
-import { FPNumber, api } from '@sora-substrate/util';
-import { DAI, XOR } from '@sora-substrate/util/assets/consts';
-import { withConnectedAccount } from './util';
+import { api } from '@sora-substrate/util';
+
+import { delay, withConnectedAccount } from './util';
 
 async function main(): Promise<void> {
   await withConnectedAccount(async () => {
@@ -8,13 +8,14 @@ async function main(): Promise<void> {
     let total = 0;
     for (let index = 0; index < num; index++) {
       const start = performance.now();
-      const res = await api.swap.getResultRpc(XOR.address, DAI.address, 100);
-      const amount = FPNumber.fromCodecValue(res.amount);
-      const amountStr = amount.toLocaleString();
+      await api.calcStaticNetworkFees();
+      if (index === 0) {
+        console.info(api.NetworkFee);
+      }
       const end = performance.now();
       const time = end - start;
       total = total + time;
-      console.info(`[#${index + 1}] amount: ${amountStr} DAI, time: ${time} ms`);
+      console.info(`[#${index + 1}]: ${time} ms`);
     }
     console.info('Average time:', total / num);
   });
