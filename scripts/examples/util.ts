0bb63b69f63baf9fdfa40bb885166d648fb6daeb
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-09-13 21:04:36 +0200

    Update util.ts

--- scripts/examples/util.ts
+++ scripts/examples/util.ts
@@ -9,22 +9,26 @@ export async function delay(ms = 40000): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function connectAndImportAccount(env = SORA_ENV.dev, mnemonic?: string): Promise<void> {
+export async function connectAndImportAccount(
+  env = SORA_ENV.stage,
+  withKeyringLoading = true,
+  mnemonic?: string
+): Promise<void> {
   await connection.open(env);
   console.info('Connected: ' + env);
-  await api.initialize();
+  await api.initialize(withKeyringLoading);
   await api.calcStaticNetworkFees();
   // salon muscle select culture inform pen typical object fox fruit culture civil
   api.importAccount(mnemonic ?? TST_MNEMONIC, 'name', 'pass');
 }
 
-async function disconnect(): Promise<void> {
+export async function disconnect(): Promise<void> {
   api.logout();
   await connection.close();
 }
 
 export async function withConnectedAccount(fn: Function, env = SORA_ENV.dev, mnemonic?: string): Promise<void> {
-  await connectAndImportAccount(env, mnemonic);
+  await connectAndImportAccount(env, true, mnemonic);
   await fn();
   await disconnect();
 }
