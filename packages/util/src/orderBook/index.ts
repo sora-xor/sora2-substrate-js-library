7076ef107820424497ad44ee6b73ae3cbb2c3d98
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2023-10-02 10:19:26 +0300

    prework

--- packages/util/src/orderBook/index.ts
+++ packages/util/src/orderBook/index.ts
@@ -1,87 +1,106 @@
 import { OrderBookOrderBookStatus } from '@polkadot/types/lookup';
+
 import type { Api } from '../api';
 
-import { DexId } from '../dex/consts';
+import { map } from 'rxjs';
+import { FPNumber } from '@sora-substrate/math';
 
 export class OrderBookModule<T> {
   constructor(private readonly root: Api<T>) {}
 
   public orderBooks = {};
+  public userOrderLimits = {};
 
   async getOrderBooks() {
     const toKey = (address) => address.code.toString();
 
     const entries = await this.root.api.query.orderBook.orderBooks.entries();
 
+    console.log('entries', entries);
+
     const orderBooks = entries.reduce((buffer, [key, value]) => {
-      const orderBook = key.args[0].toJSON();
-
-      const meta = value.unwrap();
-
-      const { orderBookId, status, lastOrderId, tickSize, stepLotSize, minLotSize, maxLotSize } = meta;
-
-      const base = orderBookId.toJSON().base;
-      const quote = orderBookId.toJSON().quote;
-
-      buffer[toKey(base) + toKey(quote)] = {
-        orderBook,
-        status: status.toString(),
-        lastOrderId: lastOrderId.toNumber(),
-        tickSize: tickSize.toNumber(),
-        stepLotSize: stepLotSize.toNumber(),
-        minLotSize: minLotSize.toBigInt(),
-        maxLotSize: maxLotSize.toBigInt(),
-      };
-
-      return buffer;
+      // const orderBook = key.args[0].toJSON();
+      // const meta = value.unwrap();
+      // const { orderBookId, status, lastOrderId, tickSize, stepLotSize, minLotSize, maxLotSize } = meta;
+      // const base = orderBookId.toJSON().base;
+      // const quote = orderBookId.toJSON().quote;
+      // buffer[toKey(base) + toKey(quote)] = {
+      //   orderBook,
+      //   status: status.toString(),
+      //   lastOrderId: lastOrderId.toNumber(),
+      //   tickSize: tickSize.toNumber(),
+      //   stepLotSize: stepLotSize.toNumber(),
+      //   minLotSize: minLotSize.toBigInt(),
+      //   maxLotSize: maxLotSize.toBigInt(),
+      // };
+      // return buffer;
     }, {});
 
     this.orderBooks = orderBooks;
   }
 
-  async getUserLimitOrders(account: string) {
-    const toKey = (address) => address.code.toString();
-
-    const entries = await this.root.api.query.orderBook.userLimitOrders.entries(account);
-
-    const userLimitOrderIds = entries.reduce((buffer, [key, value]) => {
-      const base = key.args[1].toJSON().base;
-      const quote = key.args[1].toJSON().quote;
-
-      const limitOrderIds = value.toJSON();
-
-      buffer[toKey(base) + toKey(quote)] = limitOrderIds;
-
-      return buffer;
-    }, {});
-
-    Object.entries(userLimitOrderIds);
-
-    const entry = await this.root.api.query.orderBook.limitOrders(
-      {
-        base: '0x0200040000000000000000000000000000000000000000000000000000000000',
-        quote: '0x0200000000000000000000000000000000000000000000000000000000000000',
-      },
-      2
-    );
-
-    console.log('entry', entry);
+  getAggregatedAsks(base, quote) {
+    // return this.root.apiRx.query.orderBook.aggregatedAsks({ dexId: 0, base, quote }).pipe(
+    //   map((data) => {
+    //     const asks = data.toJSON();
+    //     for (const ask of Object.keys(asks)) {
+    //       console.log('price', JSON.parse(ask).inner);
+    //       console.log('amount', asks[ask].inner);
+    //     }
+    //     return asks;
+    //   })
+    // );
   }
 
-  async getLimitOrders(base: string, quote: string) {
-    const limitOrders = await this.root.api.query.orderBook.limitOrders.entries({ base, quote });
-
-    console.log('limitOrders', limitOrders);
+  getAggregatedBids(base, quote) {
+    // return this.root.apiRx.query.orderBook.aggregatedBids({ dexId: 0, base, quote }).pipe(
+    //   map((result) => {
+    //     console.log('result bids', result.toJSON());
+    //     return result;
+    //   })
+    // );
   }
 
-  placeLimitOrder(base: string, quote: string) {
-    this.root.submitExtrinsic(
-      this.root.api.tx.orderBook.placeLimitOrder({ base, quote }, 10000, 1000, 'Buy', 9999),
-      this.root.account.pair
-    );
+  getUserLimitOrders(base, quote) {
+    // TODO: get ids first
+    // return this.root.apiRx.query.orderBook.limitOrders.multi([
+    //   [{ dexId: 0, base, quote }, 1],
+    //   [{ dexId: 0, base, quote }, 2],
+    // ]);
   }
 
-  cancelLimitOrder(base: string, quote: string) {
-    this.root.submitExtrinsic(this.root.api.tx.orderBook.cancelLimitOrder({ base, quote }, 1), this.root.account.pair);
+  async getUserLimitOrdersIds(account: string) {
+    // const toKey = (address) => address.code.toString();
+    // const entries = await this.root.api.query.orderBook.userLimitOrders.entries(account);
+    // const userLimitOrderIds = entries.reduce((buffer, [key, value]) => {
+    //   const base = key.args[1].toJSON().base;
+    //   const quote = key.args[1].toJSON().quote;
+    //   const limitOrderIds = value.toJSON();
+    //   buffer[toKey(base) + toKey(quote)] = limitOrderIds;
+    //   return buffer;
+    // }, {});
+    // return userLimitOrderIds;
   }
+
+  // public async getLimitOrdersObservable(base: string, quote: string) {
+  //   const multiEntries = [
+  //     [base, quote],
+  //     [4, 5, 6, 2],
+  //   ];
+
+  //   // const otherParams = otherAssetIds.map((assetId) => [accountId, assetId]);
+
+  //   return this.root.apiRx.query.orderBook.limitOrders.multi(multiEntries);
+  // }
+
+  // placeLimitOrder(base: string, quote: string) {
+  //   this.root.submitExtrinsic(
+  //     this.root.api.tx.orderBook.placeLimitOrder({ base, quote }, 10000, 1000, 'Buy', 9999),
+  //     this.root.account.pair
+  //   );
+  // }
+
+  // cancelLimitOrder(base: string, quote: string) {
+  //   this.root.submitExtrinsic(this.root.api.tx.orderBook.cancelLimitOrder({ base, quote }, 1), this.root.account.pair);
+  // }
 }
--- scripts/orderBook.ts
+++ scripts/orderBook.ts
@@ -1,19 +1,40 @@
 import { api } from '@sora-substrate/util';
 
-import { withConnectedAccount } from './examples/util';
+import { delay, withConnectedAccount } from './examples/util';
 
 async function main(): Promise<void> {
   await withConnectedAccount(async () => {
-    // await api.orderBook.getOrderBooks();
+    // console.log('FEE:', api.NetworkFee);
+    const xor = '0x0200000000000000000000000000000000000000000000000000000000000000';
+    const val = '0x0200040000000000000000000000000000000000000000000000000000000000';
+
+    await api.orderBook.getOrderBooks();
 
     // console.log('orderBooks', api.orderBook.orderBooks);
 
-    await api.orderBook.getUserLimitOrders('cnVkoGs3rEMqLqY27c2nfVXJRGdzNJk2ns78DcqtppaSRe8qm');
+    // const result = await api.orderBook.getUserLimitOrdersIds('cnVkoGs3rEMqLqY27c2nfVXJRGdzNJk2ns78DcqtppaSRe8qm');
 
-    // api.orderBook.getLimitOrders(
-    //   '0x0200050000000000000000000000000000000000000000000000000000000000',
-    //   '0x0200000000000000000000000000000000000000000000000000000000000000'
-    // );
+    // console.log('result ids', result);
+
+    // const subscription = api.orderBook.getLimitOrdersObservable(val, xor).subscribe((result) => {
+    //   console.log('subscription', result);
+    // });
+
+    // await api.orderBook.getLimitOrdersObservable(val, xor);
+
+    // api.orderBook.getUserLimitOrders(val, xor).subscribe((result) => {
+    //   console.log('result', result);
+    // });
+
+    // api.orderBook.getAggregatedAsks(val, xor).subscribe((asks) => {
+    //   // console.log('asks', asks);
+    // });
+
+    // api.orderBook.getAggregatedBids(val, xor).subscribe((bids) => {
+    //   console.log('bids', bids);
+    // });
+
+    await delay(100000);
   });
 }
 
