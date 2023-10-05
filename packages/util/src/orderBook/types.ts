e6b72adc756436382f2266472e7703b1ced39fb0
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-10-05 17:24:01 +0200

    Update types.ts

--- packages/util/src/orderBook/types.ts
+++ packages/util/src/orderBook/types.ts
@@ -6,17 +6,14 @@ export enum OrderBookStatus {
    * All operations are allowed
    */
   Trade,
-
   /**
    * Users can place and cancel limit order, but trading is forbidden
    */
   PlaceAndCancel,
-
   /**
    * Users can only cancel their limit orders. Placement and trading are forbidden
    */
   OnlyCancel,
-
   /**
    * All operations with order book are forbidden. Current limit orders are
    * frozen and users cannot cancel them
