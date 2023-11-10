b5346c5174a9a25a06346d453dbe2fbbd3588622
f33r0 <95526886+f33r0@users.noreply.github.com>
2023-11-10 09:38:52 +0300

    Update consts.ts

--- packages/types/scripts/consts.ts
+++ packages/types/scripts/consts.ts
@@ -1,6 +1,6 @@
 export enum SORA_ENV {
   prod = 'wss://sora.api.onfinality.io/public-ws',
   stage = 'wss://ws.framenode-7.s4.stg1.sora2.soramitsu.co.jp',
-  test = 'wss://ws.framenode-2.v1.tst.sora2.soramitsu.co.jp',
+  test = 'wss://ws.framenode-2.r0.tst.sora2.soramitsu.co.jp',
   dev = 'wss://ws.framenode-2.r0.dev.sora2.soramitsu.co.jp',
 }
