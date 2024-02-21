4b66de9bcf661fe758245efe6d47655476eaf2d7
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2024-02-21 22:51:27 +0100

    Fix Sonar issues in assets module

--- packages/util/src/assets/types.ts
+++ packages/util/src/assets/types.ts
@@ -1,6 +1,16 @@
 import type { CodecString, NumberLike } from '@sora-substrate/math';
 
 import type { History } from '../BaseApi';
+import type { KnownSymbols } from './consts';
+
+export type NativeSymbol =
+  | KnownSymbols.XOR
+  | KnownSymbols.VAL
+  | KnownSymbols.PSWAP
+  | KnownSymbols.XSTUSD
+  | KnownSymbols.XST
+  | KnownSymbols.TBCD;
+export type KnownSymbol = keyof typeof KnownSymbols;
 
 export interface WhitelistItem {
   symbol: string;
