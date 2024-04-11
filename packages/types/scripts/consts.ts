f903b97dce18743fff3b778acfa7d3fc6204bed2
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2024-04-11 19:55:14 +0300

    update stage endpoint

--- packages/types/scripts/consts.ts
+++ packages/types/scripts/consts.ts
@@ -1,6 +1,6 @@
 export enum SORA_ENV {
   prod = 'wss://sora.api.onfinality.io/public-ws',
-  stage = 'wss://ws.framenode-7.s4.stg1.sora2.soramitsu.co.jp',
+  stage = 'wss://ws.framenode-8.s5.stg1.sora2.soramitsu.co.jp',
   test = 'wss://ws.framenode-2.r0.tst.sora2.soramitsu.co.jp',
   dev = 'wss://ws.framenode-2.r0.dev.sora2.soramitsu.co.jp',
   predev = 'wss://ws.framenode-1.v1.predev.sora2.soramitsu.co.jp'
