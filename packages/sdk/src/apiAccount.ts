a06fa591b48dcf6637cab6f4af9fc823920ede53
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2024-11-11 16:12:44 +0300

    update account identity

--- packages/sdk/src/apiAccount.ts
+++ packages/sdk/src/apiAccount.ts
@@ -107,6 +107,7 @@ export class WithConnectionApi {
     const result = data.unwrap();
 
     return {
+      displayName: result.info.display.value.toHuman() as string,
       legalName: result.info.legal.value.toHuman() as string,
       approved: Boolean(result.judgements.length),
       identity: result.toHuman() as unknown as OriginalIdentity,
