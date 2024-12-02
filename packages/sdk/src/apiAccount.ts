75e5755ce57ba30b378078a80ea1ef8ebaab8182
Kron1749 <drakgr98@gmail.com>
2024-12-02 16:16:51 +0300

    reversed_to_previously_version

--- packages/sdk/src/apiAccount.ts
+++ packages/sdk/src/apiAccount.ts
@@ -249,16 +249,10 @@ export class WithAccountPair extends WithConnectionApi {
 
   public switchToMstAccount(): void {
     this.previousAccount = cloneDeep(this.account);
-    if (this.mstAccount) {
-      this.account = cloneDeep(this.mstAccount);
-    }
     this.mstActive = true;
   }
 
   public switchToMainAccount(): void {
-    if (this.previousAccount) {
-      this.account = cloneDeep(this.previousAccount);
-    }
     this.previousAccount = undefined;
     this.mstActive = false;
   }
